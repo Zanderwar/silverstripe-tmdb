ddb3b0a4afa9a491b11c0944f3bbc2aa7832919b
Zanderwar <reece.alexander@gmail.com>
2016-10-02 15:59:14 +1100

    SyncMemory model for sync interruption recovery

---
+++ code/models/SyncMemory.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Class SyncMemory
+ *
+ * Currently only used in case of sync interruption.
+ *
+ */
+class SyncMemory extends DataObject {
+    protected static $db = array(
+        "Movies" => "Int"
+    );
+}
